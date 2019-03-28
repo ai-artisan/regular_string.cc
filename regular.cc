afda1050db90d6ec6ed09cf8017016832c12b3a8
tellip <luojunyi.0@gmail.com>
2019-03-28 20:38:10 +0800

    asdf

--- regular.cc
+++ regular.cc
@@ -1,40 +1,54 @@
 #include "regular.h"
 
 namespace regular {
-    template<typename Character>
-    Pattern<Character>::Record::Record(const bool &success, const typename Traits<Character>::String::const_iterator &begin, const typename Traits<Character>::String::const_iterator &end)
+    template<typename CharacterType>
+    Traits<CharacterType>::~Traits() = default;
+
+    regular::Traits<char>::~Traits() = default;
+
+    regular::Traits<wchar_t>::~Traits() = default;
+
+    template<typename CharacterType>
+    Pattern<CharacterType>::Record::Record(const bool &success, const typename Pattern::String::const_iterator &begin, const typename Pattern::String::const_iterator &end)
             :success(success),
              begin(begin),
              end(end) {}
 
-    template<typename Character>
-    inline typename Traits<Character>::Json Pattern<Character>::Record::json() {
-        return typename Traits<Character>::String(begin, end);
+    template<typename CharacterType>
+    inline typename Pattern<CharacterType>::Json Pattern<CharacterType>::Record::json() {
+        return typename Pattern::String(begin, end);
     }
 
-    template<typename Character>
-    template<typename Derived>
-    inline std::shared_ptr<Derived> Pattern<Character>::Record::as() {
-        std::static_pointer_cast<Derived>(this->shared_from_this());
+    template<typename CharacterType>
+    template<typename DerivedType>
+    inline std::shared_ptr<DerivedType> Pattern<CharacterType>::Record::as() {
+        std::static_pointer_cast<DerivedType>(this->shared_from_this());
     }
 
     namespace pattern {
-        template<typename Character>
-        inline std::shared_ptr<typename Null<Character>::Record> Null<Character>::match(const typename Traits<Character>::String::const_iterator &begin, const typename Traits<Character>::String::const_iterator &end) {
-            Record r{true, begin, begin};
-            return std::make_shared<Record>(true, begin, begin);
+        template<typename CharacterType>
+        inline std::shared_ptr<typename Null<CharacterType>::Record> Null<CharacterType>::match(const typename Null::String::const_iterator &begin, const typename Null::String::const_iterator &end) {
+            typename Null::Record r{true, begin, begin};
+            return std::make_shared<typename Null::Record>(true, begin, begin);
         }
 
-        template<typename Character, typename Context>
-        inline std::shared_ptr<typename Singleton<Character, Context>::Record> Singleton<Character, Context>::match(const typename Traits<Character>::String::const_iterator &begin, const typename Traits<Character>::String::const_iterator &end) {
+        template<typename CharacterType, typename ContextType>
+        inline std::shared_ptr<typename Singleton<CharacterType, ContextType>::Record> Singleton<CharacterType, ContextType>::match(const typename Singleton::String::const_iterator &begin, const typename Singleton::String::const_iterator &end) {
             return (begin != end && describe(*begin, context))
-                   ? std::make_shared<Record>(true, begin, std::next(begin))
-                   : std::make_shared<Record>(false, begin, begin);
+                   ? std::make_shared<typename Singleton::Record>(true, begin, std::next(begin))
+                   : std::make_shared<typename Singleton::Record>(false, begin, begin);
         }
 
-        template<typename Character, typename Context>
-        Singleton<Character, Context>::Singleton(const decltype(describe) &describe, const Context &context):
-                describe(describe),
-                context(context) {}
+        template<typename CharacterType, typename ContextType>
+        Singleton<CharacterType, ContextType>::Singleton(decltype(describe) &&describe, ContextType &&context):
+                describe(std::move(describe)),
+                context(std::move(context)) {}
+
+        template<typename CharacterType>
+        Linear<CharacterType>::~Linear() = default;
+
+        template<typename CharacterType>
+        Linear<CharacterType>::Linear(decltype(sequence) &&sequence):
+                sequence(std::move(sequence)) {}
     }
 }
--- regular.h
+++ regular.h
@@ -9,60 +9,75 @@
 namespace regular {
     template<typename>
     struct Traits {
-        ~Traits() = delete;
+        virtual ~Traits() = 0;
     };
 
     template<>
     struct Traits<char> {
-        ~Traits() = delete;
+        virtual ~Traits() = 0;
 
+        using Character=char;
         using String=std::string;
         using Json=json::Json<double, char>;
     };
 
     template<>
     struct Traits<wchar_t> {
-        ~Traits() = delete;
+        virtual ~Traits() = 0;
 
+        using Character=wchar_t;
         using String=std::wstring;
         using Json=json::Json<double, wchar_t>;
     };
 
-    template<typename Character=char>
-    struct Pattern : std::enable_shared_from_this<Pattern<Character>> {
+    template<typename CharacterType>
+    struct Pattern : Traits<CharacterType>, std::enable_shared_from_this<Pattern<CharacterType>> {
         struct Record : std::enable_shared_from_this<Record> {
             const bool success;
-            const typename Traits<Character>::String::const_iterator begin, end;
+            const typename Pattern::String::const_iterator begin, end;
 
-            Record(const bool &, const typename Traits<Character>::String::const_iterator &, const typename Traits<Character>::String::const_iterator &);
+            Record(const bool &, const typename Pattern::String::const_iterator &, const typename Pattern::String::const_iterator &);
 
-            virtual typename Traits<Character>::Json json();
+            virtual typename Pattern::Json json();
 
             template<typename Derived>
             std::shared_ptr<Derived> as();
         };
 
-        virtual std::shared_ptr<Record> match(const typename Traits<Character>::String::const_iterator &, const typename Traits<Character>::String::const_iterator &) = 0;
+        virtual std::shared_ptr<Record> match(const typename Pattern::String::const_iterator &, const typename Pattern::String::const_iterator &) = 0;
     };
 
     namespace pattern {
-        template<typename Character>
-        struct Null : Pattern<Character> {
-            using Record=typename Null::Record;
-
-            std::shared_ptr<Record> match(const typename Traits<Character>::String::const_iterator &, const typename Traits<Character>::String::const_iterator &) final;
+        template<typename CharacterType>
+        struct Null : Pattern<CharacterType> {
+            std::shared_ptr<typename Null::Record> match(const typename Null::String::const_iterator &, const typename Null::String::const_iterator &) final;
         };
 
-        template<typename Character, typename Context=nullptr_t>
-        struct Singleton : Pattern<Character> {
-            using Record=typename Singleton::Record;
+        template<typename CharacterType, typename Context=nullptr_t>
+        struct Singleton : Pattern<CharacterType> {
+            std::shared_ptr<typename Singleton::Record> match(const typename Singleton::String::const_iterator &, const typename Singleton::String::const_iterator &) final;
 
-            std::shared_ptr<Record> match(const typename Traits<Character>::String::const_iterator &, const typename Traits<Character>::String::const_iterator &) final;
-
-            const std::function<bool(const Character &, const Context &)> describe;
+            const std::function<bool(const CharacterType &, const Context &)> describe;
             const Context context;
 
-            explicit Singleton(const decltype(describe) &, const Context & = nullptr);
+            explicit Singleton(decltype(describe) &&, Context && = nullptr);
         };
+
+        template<typename CharacterType>
+        struct Linear : Pattern<CharacterType> {
+            virtual ~Linear() = 0;
+
+            const std::list<std::pair<
+                    typename Linear::String,
+                    std::shared_ptr<Pattern<CharacterType>>
+            >> sequence;
+
+            Linear(decltype(sequence) &&);
+        };
+
+        namespace linear {
+
+
+        }
     }
 }
