c02a4514cbdb877f6420880d2bfbc21f45f278dd
tephi <luojunyi.0@gmail.com>
2020-04-09 23:28:13 +0800

    asdf

--- samples/test.cc
+++ samples/test.cc
@@ -3,16 +3,22 @@
 #include <iostream>
 
 int main() {
-    using namespace reg::shortcut::narrow;
+//    using namespace reg::shortcut::narrow;
+//
+//    auto p0 = plcs("asdf");
+//
+//    auto p = pk(pld({psa, p0}));
+//
+//    auto[b, r]=p->as<pt>()->match("1234asasdf");
+//
+//    std::cout << b << '\n'
+//              << r->string() << '\n';
 
-    auto p0 = plcs("asdf");
-
-    auto p = pk(pld({psa, p0}));
-
-    auto[b, r]=p->as<pt>()->match("1234asasdf");
-
-    std::cout << b << '\n'
-              << r->string() << '\n';
+    using namespace reg::shortcut::wide;
+    std::wstring s = L"null";
+    auto[b, r]=wplcs(L"null")->as<wpt>()->match(s);
+    std::wcout << b << L'\n'
+               << r->string().at(0) << L'\n';
 
     return 0;
 }
